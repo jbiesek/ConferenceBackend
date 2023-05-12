508b5ce18f7c4cba27dcdfb1dd96c19524e43299
jbiesek <jedrzejbiesek@gmail.com>
2023-05-12 15:55:01 +0200

    Spring initializr

---
+++ src/test/java/pl/jbiesek/conference/ConferenceApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.jbiesek.conference;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConferenceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
